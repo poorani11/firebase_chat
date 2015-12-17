9081bca38c83a85687d919f5c585faa975980a2d
poorani11 <poorani.mgr@gmail.com>
2015-12-17 16:52:01 +0530

    Added a firebase array

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
-// define our app and dependencies (remember to include firebase!)
 var app = angular.module("chatApp", ["firebase"]);
 
-// this factory returns a synchronized array of chat messages
 app.factory("chatMessages", ["$firebaseArray",
   function($firebaseArray) {
-    // create a reference to the database location where we will store our data
+    // create a reference to the database where we will store our data
     var randomRoomId = Math.round(Math.random() * 100000000);
-    var ref = new Firebase("https://angular-rudchat.firebaseio.com/" + randomRoomId);
+    var ref = new Firebase("https://docs-sandbox.firebaseio.com/af/array/full/" + randomRoomId);
 
-    // this uses AngularFire to create the synchronized array
     return $firebaseArray(ref);
   }
 ]);
 
 app.controller("ChatCtrl", ["$scope", "chatMessages",
-  // we pass our new chatMessages factory into the controller
   function($scope, chatMessages) {
     $scope.user = "Guest " + Math.round(Math.random() * 100);
 
-    // we add chatMessages array to the scope to be used in our ng-repeat
     $scope.messages = chatMessages;
 
-    // a method to create new messages; called by ng-submit
     $scope.addMessage = function() {
-      // calling $add on a synchronized array is like Array.push(),
-      // except that it saves the changes to our database!
+      // $add on a synchronized array is like Array.push() except it saves to the database!
       $scope.messages.$add({
         from: $scope.user,
-        content: $scope.message
+        content: $scope.message,
+        timestamp: Firebase.ServerValue.TIMESTAMP
       });
 
-      // reset the message input
       $scope.message = "";
     };
 
@@ -38,8 +31,9 @@ app.controller("ChatCtrl", ["$scope", "chatMessages",
     $scope.messages.$loaded(function() {
       if ($scope.messages.length === 0) {
         $scope.messages.$add({
-          from: "Admin",
-          content: "Welcome"
+          from: "Uri",
+          content: "Hello!",
+          timestamp: Firebase.ServerValue.TIMESTAMP
         });
       }
     });
